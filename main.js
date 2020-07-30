93e14c0bb1d7bdefeec3d58f7fd6b3d3d925ea80
Mehdi Neysi <dev.mehdineysi@gmail.com>
2020-07-30 19:04:04 +0430

    add main.js file to display list of cities

---
+++ main.js
@@ -0,0 +1,25 @@
+var cities = ['Ahvaz', 'Shiraz', 'Isfahan', 'Tehran']
+var listCities = document.getElementById('list-cities')
+
+function renderCities() {
+	listCities.innerHTML = ''
+	for (var i = 0; i < cities.length; i++) {
+		listCities.innerHTML +=
+			'<li class="list-group-item d-flex justify-content-between align-items-center">' +
+			cities[i] +
+			'<div>' +
+			'<a target="_blank" href="https://www.google.com/maps/search/' +
+			cities[i] +
+			',+Iran" class="badge badge-info mr-2"><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-map" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M15.817.613A.5.5 0 0 1 16 1v13a.5.5 0 0 1-.402.49l-5 1a.502.502 0 0 1-.196 0L5.5 14.51l-4.902.98A.5.5 0 0 1 0 15V2a.5.5 0 0 1 .402-.49l5-1a.5.5 0 0 1 .196 0l4.902.98 4.902-.98a.5.5 0 0 1 .415.103zM10 2.41l-4-.8v11.98l4 .8V2.41zm1 11.98l4-.8V1.61l-4 .8v11.98zm-6-.8V1.61l-4 .8v11.98l4-.8z"/></svg></a>' +
+			'<button class="badge badge-warning mr-2" onclick="editCity(' +
+			i +
+			')"><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-pencil" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M11.293 1.293a1 1 0 0 1 1.414 0l2 2a1 1 0 0 1 0 1.414l-9 9a1 1 0 0 1-.39.242l-3 1a1 1 0 0 1-1.266-1.265l1-3a1 1 0 0 1 .242-.391l9-9zM12 2l2 2-9 9-3 1 1-3 9-9z"/><path fill-rule="evenodd" d="M12.146 6.354l-2.5-2.5.708-.708 2.5 2.5-.707.708zM3 10v.5a.5.5 0 0 0 .5.5H4v.5a.5.5 0 0 0 .5.5H5v.5a.5.5 0 0 0 .5.5H6v-1.5a.5.5 0 0 0-.5-.5H5v-.5a.5.5 0 0 0-.5-.5H3z"/></svg></button>' +
+			'<button class="badge badge-danger" onclick="deleteCity(' +
+			i +
+			')"><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-dash-circle" fill="currentColor" xmlns="http://www.w3.org/2000/svg"> <path fill-rule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/> <path fill-rule="evenodd" d="M3.5 8a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.5-.5z"/></svg></button>' +
+			'</div>' +
+			'</li>'
+	}
+}
+
+renderCities()
