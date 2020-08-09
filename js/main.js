17b5d15513c001392c02796b009f4c092954e0af
Mehdi Neysi <dev.mehdineysi@gmail.com>
2020-08-09 13:07:39 +0430

    use of es6

--- js/main.js
+++ js/main.js
@@ -1,133 +1,115 @@
-var cities = ['Ahvaz', 'Shiraz', 'Isfahan', 'Tehran']
-var listCities = document.getElementById('list-cities')
-var btnClearItems = document.getElementById('btn-clear-items')
+const cities = ['Ahvaz', 'Shiraz', 'Isfahan', 'Tehran'];
 
-function renderCities() {
-	listCities.innerHTML = ''
-	for (var i = 0; i < cities.length; i++) {
-		listCities.innerHTML +=
-			'<li class="Box-row d-flex flex-items-center flex-justify-between">' +
-			cities[i] +
-			'<div>' +
-			'<a target="_blank" href="https://www.google.com/maps/search/' +
-			cities[i] +
-			',+Iran" class="btn btn-sm mr-2"><i class="fas fa-map"></i></a>' +
-			'<button class="btn btn-sm btn-outline mr-2" onclick="editCity(' +
-			i +
-			')"><i class="fas fa-edit"></i></button>' +
-			'<button class="btn btn-sm btn-danger" onclick="deleteCity(' +
-			i +
-			')"><i class="fas fa-trash"></i></button>' +
-			'</div>' +
-			'</li>'
-	}
-}
+let listCities = document.getElementById('list-cities');
+let btnClearItems = document.getElementById('btn-clear-items');
 
-function addCity() {
-	var city = document.getElementById('city')
+const renderCities = () => {
+  listCities.innerHTML = '';
 
-	if (city.value.trim() == '') {
-		return Swal.fire({
-			icon: 'warning',
-			html: 'Please enter a city.',
-			focusConfirm: false,
-			confirmButtonText: 'Ok',
-		})
-	}
+  for (var i = 0; i < citiesLength(); i++) {
+    listCities.innerHTML +=
+      '<li class="Box-row d-flex flex-items-center flex-justify-between">' +
+      cities[i] +
+      '<div>' +
+      '<a target="_blank" href="https://www.google.com/maps/search/' +
+      cities[i] +
+      ',+Iran" class="btn btn-sm mr-2"><i class="fas fa-map"></i></a>' +
+      '<button class="btn btn-sm btn-outline mr-2" onclick="editCity(' +
+      i +
+      ')"><i class="fas fa-edit"></i></button>' +
+      '<button class="btn btn-sm btn-danger" onclick="deleteCity(' +
+      i +
+      ')"><i class="fas fa-trash"></i></button>' +
+      '</div>' +
+      '</li>';
+  }
+};
 
-	if (cities.includes(city.value)) {
-		Swal.fire({
-			icon: 'error',
-			html:
-				city.value +
-				' has already been added.' +
-				'<div>Please enter another city.</div>',
-			focusConfirm: false,
-			confirmButtonText: 'Ok',
-		})
-		return (city.value = '')
-	}
+const citiesLength = () => cities.length;
 
-	cities.push(city.value)
-	city.value = ''
+const addCity = () => {
+  let city = document.getElementById('city');
 
-	isEmpty()
-	renderCities()
-}
+  if (city.value.trim() == '') {
+    return Swal.fire({
+      icon: 'warning',
+      html: 'Please enter a city.',
+      focusConfirm: false,
+      confirmButtonText: 'Ok'
+    });
+  }
 
-function deleteCity(index) {
-	var city = cities[index]
+  if (cities.includes(city.value)) {
+    Swal.fire({
+      icon: 'error',
+      html: `${city.value} has aleready been added.
+        <div>Please enter another city.</div>`,
+      focusConfirm: false,
+      confirmButtonText: 'Ok'
+    });
+    return (city.value = '');
+  }
 
-	Swal.fire({
-		title: 'Are you sure?',
-		text: "You won't be able to revert this!",
-		icon: 'warning',
-		showCancelButton: true,
-		confirmButtonColor: '#3085d6',
-		cancelButtonColor: '#d33',
-		confirmButtonText: 'Yes, delete it!',
-	}).then((result) => {
-		if (result.value) {
-			cities.splice(index, 1)
-			Swal.fire(
-				'Deleted!',
-				'This city from list has been deleted.',
-				'success'
-			)
+  cities.push(city.value);
+  city.value = '';
 
-			isEmpty()
-			renderCities()
-		}
-	})
-}
+  isEmpty();
+  renderCities();
+};
 
-function editCity(index) {
-	Swal.fire({
-		title: 'You can edit.',
-		input: 'text',
-		inputAttributes: {
-			autocapitalize: 'off',
-		},
-		showCancelButton: true,
-		confirmButtonText: 'Edit',
-	}).then((updateCity) => {
-		if (updateCity.dismiss == 'cancel') {
-			return (cities[index] = cities[index])
-		}
+const deleteCity = index => {
+  Swal.fire({
+    title: 'Are you sure?',
+    text: "You won't be able to revert this!",
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonColor: '#3085d6',
+    cancelButtonColor: '#d33',
+    confirmButtonText: 'Yes, delete it!'
+  }).then(result => {
+    if (result.value) {
+      cities.splice(index, 1);
+      Swal.fire('Deleted!', 'This city from list has been deleted.', 'success');
 
-		if (updateCity.value == '') {
-			return Swal.fire('Warning!', 'Please enter a city.', 'warning')
-		}
+      isEmpty();
+      renderCities();
+    }
+  });
+};
 
-		if (cities.includes(updateCity.value)) {
-			return Swal.fire(
-				'Warning!',
-				'This is the same as before.',
-				'warning'
-			)
-		}
+const editCity = index => {
+  Swal.fire({
+    title: 'You can edit.',
+    input: 'text',
+    inputAttributes: {
+      autocapitalize: 'off'
+    },
+    showCancelButton: true,
+    confirmButtonText: 'Edit'
+  }).then(editedCity => {
+    if (editedCity.dismiss == 'cancel') return (cities[index] = cities[index]);
+    if (editedCity.value == '')
+      return Swal.fire('Warning!', 'Please enter a city.', 'warning');
+    if (cities.includes(editedCity.value))
+      return Swal.fire('Warning!', 'This is the same as before.', 'warning');
 
-		cities[index] = updateCity.value
-		Swal.fire('Edited!', 'The previous city was edited.', 'success')
+    cities[index] = editedCity.value;
+    Swal.fire('Edited!', 'The previous city was edited.', 'success');
 
-		isEmpty()
-		renderCities()
-	})
-}
+    isEmpty();
+    renderCities();
+  });
+};
 
-function clearItems() {
-	cities.splice(0, cities.length)
-	btnClearItems.innerHTML = ''
+const clearItems = () => {
+  cities.splice(0, cities.length);
+  btnClearItems.innerText = '';
+  renderCities();
+};
 
-	renderCities()
-}
+const isEmpty = () => {
+  if (citiesLength() > 0) btnClearItems.innerText = 'Clear items';
+  else btnClearItems.innerText = '';
+};
 
-function isEmpty() {
-	if (cities.length > 0) {
-		btnClearItems.innerHTML = 'Clear items'
-	} else {
-		btnClearItems.innerHTML = ''
-	}
-}
-
-renderCities()
+renderCities();
